8b6fb716452d819db62d80f68ec25ea2cf774ba0
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-09-18 12:41:22 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+//Problem1
+//    double x;
+//    cin>>x;
+//    if (x>-1)
+//        cout<<"Positive"<<endl;
+//    else
+//        cout<<"Negative"<<endl;
+//    return 0;
+
+//Problem2
+//    double x;
+//    cin>>x;
+//    if (x>-1)
+//        if (x%2==0)
+//            cout<<"The number is positive and even"<<endl;
+//        else
+//            cout<<"The number is positive and odd"<<endl;
+//    else
+//        if (x%2==0)
+//            cout<<"The number is negative and even"<<endl;
+//        else
+//            cout<<"The number is negative and odd"<<endl;
+//    return 0;
+
+//Problem3
+//    double x,y,z;
+//    cin>>x;
+//    cin>>y;
+//    cin>>z;
+//    if (x>y && x>z)
+//        cout<<x<<" is the largest"<<endl;
+//    else
+//        if (y>z && y>x)
+//            cout<<y<<" is the largest"<<endl;
+//        else
+//            cout<<z<< " is the largest"<<endl;
+
+//Problem4
+//    int x;
+//    int y;
+//    cin>>x;
+//    cin>>y;
+//    if (x%y==0)
+//        cout<<x<<"is devisible by "<<y<<endl;
+//    else
+//        cout<<x<<"is not devisible by "<<y<<endl;
+
+//Problem5
+//    int x;
+//    cin>>x;
+//    if (x%4==0 && x%100!=0 && x%400!=0)
+//            cout<<x<<" is  a leap year"<<endl;
+//    else
+//        if (x==2000)
+//            cout<<x<<" is  a leap year"<<endl;
+//        else
+//            cout<<x<<" is not leap year"<<endl;
+
+//Problem6
+//    double a;
+//    double b;
+//    double c;
+//    cin>>a;
+//    cin>>b;
+//    cin>>c;
+//    cout<<"x1= "<<((-b)+((b*b-4*a*c)**0.5)/2*a)<<endl;
+//    cout<<"x2= "<<((-b)-((b*b-4*a*c)**0.5)/2*a)<<endl;
+
+//Problem7
+//    int a,b,c;
+//    cin>>a;
+//    cin>>b;
+//    cin>>c;
+//    if (a+b+c==180)
+//        cout<<"The triangle is valid"<<endl;
+//    else
+//        cout<<"The triangle is not valid"<<endl;
+
+//Problem8
+//    char a;
+//    cin>>a;
+//    if (int(a)>65 && int(a)<90)
+//        cout<<"Upper"<<endl;
+//    else
+//        if (int(a)>97 && int(a)<122)
+//            cout<<"Lower"<<endl;
+//        else
+//            cout<<"not defined"<<endl;
+
+}
